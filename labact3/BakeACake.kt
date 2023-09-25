3df081a08013a3a24350751235ff125eae0b278e
eyrogel <145763139+eyrogel@users.noreply.github.com>
2023-09-25 17:26:03 +0800

    Create BakeACake.kt

---
+++ labact3/BakeACake.kt
@@ -0,0 +1,36 @@
+fun main() { 
+    val age = 21
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
+}
+
+fun printCakeCandles(age:Int) {
+    print(" ")
+    repeat(age) {
+        print("'")
+    }
+    println()
+    print(" ")
+    repeat(age) {
+        print("|")
+    }
+    println()
+}
+
+fun printCakeTop(age:Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age:Int, layers:Int) {
+    repeat(layers) {
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
+}
